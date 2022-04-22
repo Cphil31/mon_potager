ff61a30691996a38de7d6ca7185cb20f241d3fdf
Cphil31 <cecilephilippe31@gmail.com>
2022-04-22 16:24:32 +0200

    login and logout

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class HomeController extends AbstractController
 {
@@ -15,4 +19,32 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    #[Route('/new_user', name: 'new_user')]
+    public function newUser(ManagerRegistry $doctrine,UserPasswordHasherInterface $passwordHasher): Response {
+
+        $entityManager = $doctrine->getManager();
+
+        $user = new User();
+        $user->setEmail('toto@gmail.com');
+        $plaintextPassword = "toto";
+
+        // hash the password (based on the security.yaml config for the $user class)
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($user);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$user->getId());
+
+    }
+
 }
